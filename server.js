ddfbe0868491f415caf847750932c6df9aaaf944
Andrew G Burkus <da8urk@gmail.com>
2015-02-15 04:58:35 -0600

    huge update

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 io.on('connection', function(socket) {
 	console.log("connection established son!");
+	socket.on('update', function() {
+
+	});
+});
+
+app.use(express.static('public/'));
+app.get('/', function(req, res) {
+	res.sendfile("public/main.html");
 });
 
 var port = process.env.port || 8080;
 http.listen(port, function() {
 	console.log('Magic Hands on port: ' + port);
-})
+});
